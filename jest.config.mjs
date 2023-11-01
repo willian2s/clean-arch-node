29b19a659d553698702ffe40c1cf07d9cacce54d
Willian Silva <silva.williansantos@gmail.com>
2023-11-01 11:35:49 -0300

    chore: :technologist: add jest

---
+++ jest.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node'
+}
+
+module.exports = config
