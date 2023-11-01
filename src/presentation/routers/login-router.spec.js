352a3858a05ab35c6e0cf979619206c4ceac59c0
Willian Silva <silva.williansantos@gmail.com>
2023-11-01 16:00:35 -0300

    test: remove duplicated code

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -9,7 +9,8 @@ class LoginRouter {
    * @returns {HttpResponse}
    */
   route (httpRequest) {
-    if (!httpRequest.body.email || !httpRequest.body.password) {
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
