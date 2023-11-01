12dc5fd36b9ce51287fedc53c37e9afa0ac6ed6c
Willian Silva <silva.williansantos@gmail.com>
2023-11-01 15:19:31 -0300

    chore: :technologist: add jest on lint-staged

--- src/type.js
+++ src/type.js
@@ -4,9 +4,6 @@
  * @property {Date} created_at
  * @property {Date} updated_at
  *
- * @typedef {Object} HttpRequest
- * @property {object} HttpRequest.body
- *
  * @typedef {Object} HttpResponse
  * @property {object} HttpResponse.body
  * @property {number} HttpResponse.statusCode
