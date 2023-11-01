fd79cd8149cf4f85175974cfc4521880b3136721
Willian Silva <silva.williansantos@gmail.com>
2023-11-01 15:17:03 -0300

    test: :white_check_mark: ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,11 +1,15 @@
 class LoginRouter {
   /**
+   * @typedef {Object} HttpRequest
+   * @property {object} HttpRequest.body
+   * @property {string} HttpRequest.body.email
+   * @property {string} HttpRequest.body.password
    *
    * @param {HttpRequest} httpRequest
    * @returns {HttpResponse}
    */
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -19,6 +23,17 @@ describe('Login Router', () => {
     const httpRequest = {
       body: {
         password: 'anypassword'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should returs 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any@mail.com'
       }
     }
     const httpResponse = sut.route(httpRequest)
