5dd005d7b522657f664527df97e7c1a6c3bf01fa
Willian Silva <silva.williansantos@gmail.com>
2023-11-27 10:02:14 -0300

    test(token-generator): ensure TokenGenerator returns null if JWT returns null

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,22 @@
+class TokenGenerator {
+  /**
+   *
+   * @param {string} id
+   * @returns {Promise<any>}
+   */
+  async generate (id) {
+    return null
+  }
+}
+
+const makeSut = () => {
+  return new TokenGenerator()
+}
+
+describe('Token Generator', () => {
+  test('should return null if JWT returns null', async () => {
+    const sut = makeSut()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+})
